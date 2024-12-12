dcd52ad22af1a4ba532798ce9411e3c7837a8e3a
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-12-12 09:22:07 +0700

    admin chỉnh sửa sản phẩm

--- routers/admin/product.router.js
+++ routers/admin/product.router.js
@@ -6,22 +6,20 @@ const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const uploadCloud = require("../../middlewares/admin/upload.middleware");
-
 const validate = require("../../validates/admin/product.validate");
 
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
 
-router.post(
-    "/create",
-    upload.single('thumbnail'),
-    uploadCloud.upload,
-    validate.creatPost,
-    controller.createPost
-);
+router.post("/create", upload.single('thumbnail'), uploadCloud.upload, validate.creatPost, controller.createPost);
 
+router.get("/detail/:id", controller.detail);
 
+router.get("/edit/:id", controller.edit);
 
+router.patch("/edit/:id", upload.single('thumbnail'), uploadCloud.upload, validate.creatPost, controller.editPatch);
+
+// router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
